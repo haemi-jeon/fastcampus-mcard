2a8e111bb9b74b45404ff0ee08199196a42edcf5
hmJeon <hmjeon@aiblab.co.kr>
2024-09-04 08:30:57 +0900

    카드 상세페이지 애니메이션 구현

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 import { getCard } from '@/remote/card'
 import Top from '@/components/shared/Top'
@@ -32,16 +33,34 @@ function CardPage() {
       <ul>
         {benefit.map((text, index) => {
           return (
-            <ListRow
-              key={text}
-              left={<IconCheck />}
-              contents={
-                <ListRow.Texts title={`혜택 ${index + 1}`} subTitle={text} />
-              }
-            />
+            <motion.li
+              initial={{
+                opacity: 0,
+                translateX: -90,
+              }}
+              transition={{
+                duration: 0.7,
+                ease: 'easeInOut',
+                delay: index * 0.1,
+              }}
+              animate={{
+                opacity: 1,
+                translateX: 0,
+              }}
+            >
+              <ListRow
+                as="div"
+                key={text}
+                left={<IconCheck />}
+                contents={
+                  <ListRow.Texts title={`혜택 ${index + 1}`} subTitle={text} />
+                }
+              />
+            </motion.li>
           )
         })}
       </ul>
+
       {promotion != null ? (
         <Flex direction="column" css={termsContainerStyles}>
           <Text bold={true}>유의사항</Text>
