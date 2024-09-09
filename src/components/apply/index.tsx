54c9ccc56f497e6eb0741c8fe0da315ebfc8ef34
hmJeon <hmjeon@aiblab.co.kr>
2024-09-09 11:06:25 +0900

    카드신청구현 - 카드신청

---
+++ src/components/apply/index.tsx
@@ -0,0 +1,32 @@
+import { ApplyValues } from '@/models/apply'
+import Terms from '@/components/apply/Terms'
+import BasicInfo from '@/components/apply/BasicInfo'
+import CardInfo from '@/components/apply/CardInfo'
+
+function Apply({ step, onSubmit }: { step: number; onSubmit: () => void }) {
+  const handleTermsChange = (terms: ApplyValues['terms']) => {
+    console.log('terms', terms)
+  }
+
+  const handleBasicInfoChange = (
+    infoValues: Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>,
+  ) => {
+    console.log('infoValues', infoValues)
+  }
+
+  const handleCardInfoChange = (
+    cardInfoValues: Pick<ApplyValues, 'isMaster' | 'isHipass' | 'isRf'>,
+  ) => {
+    console.log('cardInfoValues', cardInfoValues)
+  }
+
+  return (
+    <div>
+      {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
+      {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
+      {step === 2 ? <CardInfo onNext={handleCardInfoChange} /> : null}
+    </div>
+  )
+}
+
+export default Apply
