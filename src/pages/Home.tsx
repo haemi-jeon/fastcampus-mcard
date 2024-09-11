a246a7106f598db6c7cd3896b3b3f83b7e3bfba1
hmJeon <hmjeon@aiblab.co.kr>
2024-09-11 14:38:30 +0900

    병목코드 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,9 +13,7 @@ function HomePage() {
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤습니다"
       />
-      <Button>하이</Button>
       <AdBanners />
-      <Button>하이</Button>
       <Suspense
         fallback={[...new Array(10)].map((_, idx) => (
           <ListRow.Skeleton key={idx} />
@@ -23,7 +21,6 @@ function HomePage() {
       >
         <CardList />
       </Suspense>
-      <Button>하이</Button>
     </div>
   )
 }
