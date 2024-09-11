48173f185c44d217e884b7ecba0a02d139afc8ca
hmJeon <hmjeon@aiblab.co.kr>
2024-09-11 13:42:20 +0900

    layout shift 피하는 방법

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import Top from '@/components/shared/Top'
 import AdBanners from '@/components/home/AdBanners'
 import CardList from '@/components/home/CardList'
+import Button from '@/components/shared/Button'
+import { Suspense } from 'react'
+import Skeleton from '@/components/shared/Skeleton'
+import ListRow from '@/components/shared/ListRow'
 
 function HomePage() {
   return (
@@ -9,8 +13,17 @@ function HomePage() {
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤습니다"
       />
+      <Button>하이</Button>
       <AdBanners />
-      <CardList />
+      <Button>하이</Button>
+      <Suspense
+        fallback={[...new Array(10)].map((_, idx) => (
+          <ListRow.Skeleton key={idx} />
+        ))}
+      >
+        <CardList />
+      </Suspense>
+      <Button>하이</Button>
     </div>
   )
 }
